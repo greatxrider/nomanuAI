a03c14e41d212f025df1b969e553ca1df27399df
greatxrider <daligdig.jephmari@gmail.com>
2025-09-05 21:07:12 +0800

    update

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.supabase.co https://maps.googleapis.com; frame-src https://js.stripe.com;",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.supabase.co https://maps.googleapis.com; frame-src https://js.stripe.com https://lottie.host;",
                     },
                 ],
             },
